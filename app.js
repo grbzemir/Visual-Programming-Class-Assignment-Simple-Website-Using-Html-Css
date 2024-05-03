ef91a021a2a892e4e2657d69056c1a43f98a3677
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-05-04 00:46:17 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const path = require('path');
+// const config = require('./config/database');
